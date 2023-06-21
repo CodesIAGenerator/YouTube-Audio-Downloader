0af3753b26e8d0f4beada5d941afa2fa97b49d82
CodesAIGenerator <baudirguanche@gmail.com>
2023-06-21 16:32:34 +0100

    Se han creado funciones para cada caso que elige el usuario

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 from pytube import YouTube
-# Pide la URL del video
-url = input("Ingresa la URL del video: ")
-# Crea el objeto YouTube
-yt = YouTube(url)
-# Obtener el audio del video
-stream = yt.streams.filter(only_audio=True).first()
-# Descargar el archivo de audio
-stream.download()
+
+
+# Envuelve el codigo de arriba en una funcion
+def descargar_audio(url):
+    yt = YouTube(url)
+    stream = yt.streams.filter(only_audio=True).first()
+    stream.download()
+    print("Descarga completada!!")
+
+# Crear funcion para descargar video
+def descargar_video(url):
+    yt = YouTube(url)
+    stream = yt.streams.get_highest_resolution()
+    stream.download()
+    # Mostrar barra de progreso de la descarga en porcentaje
+    print("Descarga completada!!")
+
+
+# Crea un menu con bucle while para que el usuario pueda descargar varios videos
+while True:
+    print("Bienvenido a Youtube Downloader")
+    print("1. Descargar audio")
+    print("2. Descargar video")
+    print("3. Salir")
+    opcion = int(input("Ingrese una opcion: "))
+    if opcion == 1:
+        url = input("Ingrese la URL del video: ")
+        descargar_audio(url)
+    elif opcion == 2:
+        url = input("Ingrese la URL del video: ")
+        descargar_video(url)
+    elif opcion == 3:
+        break
+    else:
+        print("Opcion no valida")
 
