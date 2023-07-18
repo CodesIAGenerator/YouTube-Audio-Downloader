9c832ecc5b54f7bc9fffa5598ede58edea342a41
CodesIAGenerator <133957888+CodesIAGenerator@users.noreply.github.com>
2023-07-18 15:25:07 +0100

    Add files via upload
    Se ha mejorado el código, ahora se muestra una barra de progreso para saber cuanto queda de descarga.

--- main.py
+++ main.py
@@ -1,37 +1,9 @@
-from pytube import YouTube
+import pytube
+from pytube.cli import on_progress
+from pytube import YouTube as YT
 
+url_youtube = input("Introduce la url del video: ")
 
-# Envuelve el codigo de arriba en una funcion
-def descargar_audio(url):
-    yt = YouTube(url)
-    stream = yt.streams.filter(only_audio=True).first()
-    stream.download()
-    print("Descarga completada!!")
+yt =YT (url_youtube,on_progress_callback=on_progress)
 
-# Crear funcion para descargar video
-def descargar_video(url):
-    yt = YouTube(url)
-    stream = yt.streams.get_highest_resolution()
-    stream.download()
-    # Mostrar barra de progreso de la descarga en porcentaje
-    print("Descarga completada!!")
-
-
-# Crea un menu con bucle while para que el usuario pueda descargar varios videos
-while True:
-    print("Bienvenido a Youtube Downloader")
-    print("1. Descargar audio")
-    print("2. Descargar video")
-    print("3. Salir")
-    opcion = int(input("Ingrese una opcion: "))
-    if opcion == 1:
-        url = input("Ingrese la URL del video: ")
-        descargar_audio(url)
-    elif opcion == 2:
-        url = input("Ingrese la URL del video: ")
-        descargar_video(url)
-    elif opcion == 3:
-        break
-    else:
-        print("Opcion no valida")
-
+yt.streams.get_audio_only().download()
